b8bbca96a315fca5a7ef067e4a40f7e3641ef2df
Jackie Spiess <Jackiespiess22@gmail.com>
2021-10-08 17:17:47 -0500

    GET route complete

--- server/server.js
+++ server/server.js
@@ -13,13 +13,22 @@ const pool = require( './modules/pool');
 
 // listening requests to port
 app.listen(PORT, ()=> {
-    console.log('listening on port:', PORT);
+console.log('listening on port:', PORT);
 });
 
 //routes 
 
 //GET
-
+// ROUTES
+app.get('/tasks', (req, res)=>{
+    const queryString = `SELECT * FROM tasks`;
+    pool.query( queryString ).then( (results)=> {
+      res.send(results.rows );
+    }).catch( (err )=>{
+      console.log( err);
+      res.sendStatus( 500);
+    })
+  })
 //POST
 
 //UPDATE
